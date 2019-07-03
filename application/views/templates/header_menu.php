feef39ee9b0537ee01fcbe5941ee027fcc7b3b01
Raja Sardar <rj.raj@outlook.com>
2019-07-03 19:54:02 +0530

    First Commit

---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,18 @@
+<header class="main-header">
+    <!-- Logo -->
+    <a href="/stock" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>MS</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>MARKESTAR.COM</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  
